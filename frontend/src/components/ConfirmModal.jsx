95585d840f7d1a3315c65ca5c9fd7bac12ec93f2
Ricardo Maroquio <maroquio@gmail.com>
2022-11-26 09:31:24 -0300

    Exclusão de aluno funcionando com mensagens HTML sanitizadas e data de nascimento sendo mostrada no formato correto.

--- frontend/src/components/ConfirmModal.jsx
+++ frontend/src/components/ConfirmModal.jsx
@@ -1,3 +1,7 @@
+import parse from "html-react-parser";
+import purify from "dompurify";
+//https://vhudyma-blog.eu/render-raw-html-in-react/
+
 const ConfirmModal = ({ question, action }) => {
     return (
         <div className="modal fade" id="confirmModal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -9,7 +13,7 @@ const ConfirmModal = ({ question, action }) => {
                         </h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div className="modal-body">{question}</div>
+                    <div className="modal-body">{parse(purify.sanitize(question))}</div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
                             Não
--- frontend/src/components/TableAlunos.js
+++ frontend/src/components/TableAlunos.js
@@ -1,37 +1,39 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import ConfirmModal from "./ConfirmModal";
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
+import ConfirmModal from "./ConfirmModal";
+import InformModal from "./InformModal";
 
 const TableAlunos = ({ alunos, setAlunos }) => {
     const [alunoExcluir, setAlunoExcluir] = useState(null);
     const [modal, setModal] = useState(undefined);
 
-    function confirmarExclusao(cliente) {
-        setAlunoExcluir(cliente);
+    function confirmarExclusao(aluno) {
+        setAlunoExcluir(aluno);
         const confirmModal = new bootstrap.Modal("#confirmModal", {});
         setModal(confirmModal);
         confirmModal.show();
     }
 
     function excluirAluno() {
-        axios.delete(`http://localhost:8080/api/alunos/${alunoExcluir._id}`).then((response) => {
-            response.data
-                .then((data) => {
-                    const alunosAtualizados = alunos.filter((aluno) => aluno._id !== alunoExcluir._id);
-                    setAlunos(alunosAtualizados);
-                    modal.hide();
-                })
-                .catch((error) => {
-                    console.log(error);
-                    modal.hide();
-                });
-        });
+        axios
+            .delete(`http://localhost:8080/api/alunos/${alunoExcluir._id}`)
+            .then((data) => {
+                const alunosAtualizados = alunos.filter((aluno) => aluno._id !== alunoExcluir._id);
+                setAlunos(alunosAtualizados);
+                modal.hide();
+                const informModal = new bootstrap.Modal("#informModal", {});
+                informModal.show();
+            })
+            .catch((error) => {
+                console.log(error);
+                modal.hide();
+            });
     }
 
     return alunos.length === 0 ? (
-        <div className="alert alert-info">Nenhum cliente cadastrado</div>
+        <div className="alert alert-info">Nenhum aluno cadastrado</div>
     ) : (
         <>
             <table className="table table-striped">
@@ -49,7 +51,7 @@ const TableAlunos = ({ alunos, setAlunos }) => {
                     {alunos.map((aluno) => (
                         <tr key={aluno._id}>
                             <td>{aluno.nome}</td>
-                            <td>{aluno.dataNascimento}</td>
+                            <td>{new Date(aluno.dataNascimento).toLocaleDateString("pt-br")}</td>
                             <td>{aluno.email}</td>
                             <td>{aluno.sexo === "M" ? "Masculino" : "Feminino"}</td>
                             <td>{aluno.ativo ? "Ativo" : "Inativo"}</td>
@@ -66,7 +68,8 @@ const TableAlunos = ({ alunos, setAlunos }) => {
                 </tbody>
             </table>
 
-            <ConfirmModal question={`Deseja realmente excluir o cliente ${alunoExcluir?.nome}?`} action={excluirAluno} />
+            <ConfirmModal question={`Deseja realmente excluir o aluno <b>${alunoExcluir?.nome}</b>?`} action={excluirAluno} />
+            <InformModal message={`Aluno <b>${alunoExcluir?.nome}</b> excluído com sucesso.`} />
         </>
     );
 };
